a8848561900e745d976c61b9b28f83a597ba48b4
dmeckley <34632714+dmeckley@users.noreply.github.com>
2019-02-26 18:21:21 -0500

    Python Geometry Modules
    Python program which calculates geometrics of circle and rectangle using modules.

---
+++ geometry.py
@@ -0,0 +1,43 @@
+import circle
+import rectangle
+
+AREA_CIRCLE_CHOICE = 1
+CIRCUMFERENCE_CHOICE = 2
+AREA_RECTANGLE_CHOICE = 3
+PERIMETER_RECTANGLE_CHOICE = 4
+QUIT_CHOICE = 5
+
+def main():
+    choice = 0
+    while choice != QUIT_CHOICE:
+        display_menu()
+        choice = int(input('Enter your choice: '))
+        if choice == AREA_CIRCLE_CHOICE:
+            radius = float(input('Enter the circle\'s radius: '))
+            print('The area is', circle.area(radius), sep=' ')
+        elif choice == CIRCUMFERENCE_CHOICE:
+            radius = float(input('Enter the circle\'s radius: '))
+            print('The circumference is', circle.circumference(radius), sep=' ')
+        elif choice == AREA_RECTANGLE_CHOICE:
+            width = float(input('Enter the rectangle\'s width: '))
+            length = float(input('Enter the rectangle\'s length: '))
+            print('The area is', rectangle.area(width, length), sep=' ')
+        elif choice == PERIMETER_RECTANGLE_CHOICE:
+            width = float(input('Enter the rectangle\'s width: '))
+            length = float(input('Enter the rectangle\'s length '))
+            print('The perimeter is', rectangle.perimeter(width, length), sep=' ')
+        elif choice == QUIT_CHOICE:
+            print('Exiting the program!')
+        else:
+            print('Error!  Invalid selection.')
+
+def display_menu():
+    print('MENU')
+    print('1) Area of circle')
+    print('2) Circumference of circle')
+    print('3) Area of rectangle')
+    print('4) Perimeter of rectangle')
+    print('5) Quit')
+
+if __name__ == "__main__":
+    main()
